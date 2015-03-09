7face61bfa1b48160f1063dc0226341a6d28e343
Matthew Turner <maturner01@gmail.com>
2015-03-08 20:02:50 -0700

    d=2 working for json built like http://json.org/example

--- make_form.js
+++ make_form.js
@@ -7,50 +7,97 @@ var suggested = d3.select("body")
 var merely_esoteric = d3.select("body")
                         .append("form");
 
+function httpGet(theUrl)
+{
+    var xmlHttp = null;
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.responseText);
+}
+
 // implicitly my rules and this sample data are saying, "if a string is a value
 // at a level, then the string is an entry. If there's an array, then its 
 // elements' values are entries. If there's an object, that is an indication of
 // a deepening hierarcy
-var mandatory_fields_default = [
+//
+// TODO: Does this make any sense?
+//
+//var mandatory_fields_default = [
+  ////metadata point of contact
+//{"organisationName": "Point of contact for metadata record"},
+//{"title": "Should inform content as well as context"},
+  //// TODO: encapsulate and format for levels of nesting //
+//{"CI_Date":
+    //[{"date": "2015-01-25T09:30:47"},
+     //{"dateType": "publication"}]
+//},
+//// has depth of two
+//{"Other stuff":
+    //[ {"thing 1": "yoyoy"},
+       //{"deepest": 
+           //[
+              //{"machine": "rage"}, 
+              //{"manu": "chao"}
+           //]
+       //}
+    //] // next put another array of objects in here for a second hierarchical level.
+//},
+//{"fake field": "Homer Simpson"},
+//{"even more fake": "Marge Simpson"}
+//]; 
+var url = "http://129.24.196.43///apps/my_app/search/datasets.json?version=3"
+//var mandatory_fields_default = httpGet(url);
+//mandatory_fields_default = mandatory_fields_default.results;
+
+// using examples `from http://json.org/example <http://json.org/example>`_
+var proper_json =
+{
   //metadata point of contact
-{"organisationName": "Point of contact for metadata record"},
-{"title": "Should inform content as well as context"},
-  // TODO: encapsulate and format for levels of nesting //
-{"CI_Date":
-    [{"date": "2015-01-25T09:30:47"},
-     {"dateType": "publication"}]
-},
-// has depth of two
-{"Other stuff":
-    [ {"thing 1": "yoyoy"},
-       {"deepest": 
-           [
-              {"machine": "rage"}, 
-              {"manu": "chao"}
-           ]
-       }
-    ] // next put another array of objects in here for a second hierarchical level.
-},
-{"fake field": "Homer Simpson"},
-{"even more fake": "Marge Simpson"}
-]; 
+  "organisationName": "Point of contact for metadata record",
+  "title": "Should inform content as well as context",
+    // TODO: encapsulate and format for levels of nesting //
+  "CI_Date":
+      {
+        "date": "2015-01-25T09:30:47",
+        "dateType": "publication"
+      },
+  // has depth of two
+  "Other stuff":
+    { 
+      "thing 1": "yoyoy",
+      "deepest": 
+        {
+          "machine": "rage",
+          "manu": "chao"
+        }
+    },
+  "fake field": "Homer Simpson",
+  "even more fake": "Marge Simpson"
+}; 
+
+console.log(proper_json)
 
 var fields = ["orgName", "title", "date"];
 
-console.log(mandatory_fields_default);
+console.log(proper_json);
 
 var form = d3.select("#mdform")
              .append("ul")
              .html("<h1 class='level0'>level0: yo</h1>");
 
 var first_key = true;
-for (var i=0; i < mandatory_fields_default.length; i++)
+var keys = d3.keys(proper_json);
+for (var i=0; i < keys.length; i++)
 {
-  var key = d3.keys(mandatory_fields_default[i])[0];
-  var val = d3.values(mandatory_fields_default[i])[0];
+  var key = keys[i];
+  console.log(key);
+  var val = proper_json[key];
+  console.log(val);
   // TODO for more nesting, can put another check for an Object, or maybe 
   // need a while for arbitrarily deep nesting. If it's another object 
-  if (val instanceof Array)
+  if (val instanceof Object)
   {
       // append a title to this section
       console.log(key);
@@ -81,13 +128,14 @@ for (var i=0; i < mandatory_fields_default.length; i++)
                  "id": "hierarchy" + i   
           });
 
-      for (var j=0; j < val.length; j++)
+      keys2 = d3.keys(val);
+      for (var j=0; j < keys2.length; j++)
       {
-        var key2 = d3.keys(val[j])[0];
-        var val2 = d3.values(val[j])[0];
+        var key2 = keys2[j];
+        var val2 = val[key2];
 
         // second hierarchical level 
-        if (val2 instanceof Array)
+        if (val2 instanceof Object)
         {
           d3.select("#hierarchy" + i)
             .append("g")
@@ -105,10 +153,11 @@ for (var i=0; i < mandatory_fields_default.length; i++)
                   "id": "hierarchy" + i + "-" + j
               }); 
 
-          for (var k=0; k < val2.length; k++)
+          keys3 = d3.keys(val2);
+          for (var k=0; k < keys3.length; k++)
           {
-              var key3 = d3.keys(val2[k])[0];
-              var val3 = d3.values(val2[k])[0];
+              var key3 = keys3[k];
+              var val3 = val2[key3];
 
               form.select("#hierarchy" + i + "-" + j)
                   .append("li")
