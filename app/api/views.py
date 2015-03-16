eb07875c59fbe909684330a196b0ec4480670750
Matthew Turner <maturner01@gmail.com>
2015-03-16 02:20:59 -0700

    view of default template, all md records, and XML route exposed and working

--- app/api/views.py
+++ app/api/views.py
@@ -1,7 +1,9 @@
 """API"""
-from flask import request
+from flask import request, jsonify, Response
 # from flask_restful import Resource, reqparse
 from flask_cors import cross_origin
+
+from dicttoxml import dicttoxml
 
 from . import api
 from .. import db
@@ -16,11 +18,16 @@ def metadata():
 
     if request.method == 'GET':
         # creates 'jsonify'd Response; default code is 200, correct for this
-        return Metadata.to_json()
+        metadata = Metadata.query.all()
+
+        return jsonify({'records': [
+            {k: v for k, v in m.__dict__.iteritems()
+                if (k != '_sa_instance_state' and k != 'id')}
+            for m in metadata]
+            })
 
     # create a new metadata record
     if request.method == 'POST':
-        print "\n\n*****  IN POST *****\n\n"
         print request.form
         newmd = Metadata.from_json(request.form)
 
@@ -28,8 +35,74 @@ def metadata():
         db.session.commit()
 
         # return a JSON record of new metadata to load into the page
-        return Metadata.to_json(newmd.id)
-
-    if request.method == 'PUT':
-        # TODO Is this where modifications should go?
         return Metadata.to_json()
+
+
+@api.route('/api/metadata/<int:id>')
+@cross_origin(origin='*', methods=['GET'])
+def get_single_metadata(id):
+    """Get the common XML representation of the metadata record with
+       given id.
+    """
+    record = Metadata.query.get_or_404(id)
+
+    return jsonify(record)
+
+
+@api.route('/api/metadata/<int:id>/xml')
+@cross_origin(origin='*', methods=['GET'])
+def get_xml_metadata(id):
+    """Get the common XML representation of the metadata record with
+       given id.
+    """
+    record = Metadata.query.get_or_404(id)
+
+    return Response(dicttoxml({k: v for k, v in record.__dict__.iteritems()
+                               if (k != '_sa_instance_state' and k != 'id')}),
+                    200, mimetype='application/xml')
+
+
+@api.route('/api/metadata/form')
+@cross_origin(origin='*', methods=['GET', 'POST', 'PUT'],
+              headers=['X-Requested-With', 'Content-Type', 'Origin'])
+def get_form():
+    return jsonify(form=_make_mdform())
+
+
+def _make_mdform(md_id=None):
+    """Make metadata form for display on client.
+
+        Args:
+            md_id (int): integer id of particular metadata record
+
+        Returns:
+            (list) of metadata blocks for display in Bootstrap panels
+
+    """
+    # TODO query Metadata for that record and fill in info. If None, read
+    # defaults from some default store.
+    return [dict(blockTitle="Basic Information", blockLabel="basic-information",
+            expanded="false",
+            formElements=[
+
+                dict(label='Title', type='text', order=1,
+                     value='Title of my data', name='title',
+                     longDescription="E.g., Data for June 2014 Journal of Hydrology Paper"),
+                dict(label='Date', type='date', order=1, name='date',
+                      value='2015-01-01',  # TODO strfmt now()
+                      longDescription='Date data was last updated')]
+         ),
+
+        dict(blockTitle="Researcher Information", blockLabel="researcher-information",
+         expanded="true",
+         formElements=[
+
+             dict(label='Researcher Name', type='text', order=1, name='rname',
+                  value='Professor Herod Jones',
+                  longDescription='Principal Investigator (may be one of many)'),
+
+             dict(label='Researcher Institute', type='text', order=1,
+                  name='rinst',
+                  value='University of Idaho',  # TODO strfmt now()
+                  longDescription='Home institute during data acquisition')]
+         )]
--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class Metadata(db.Model):  # Address, SpatialExtent, TemporalExtent, Address):
 
     ### Basic ###
     title = db.Column(db.String(200), index=True)
-    date = db.Column(db.DateTime, index=True)
+    date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     ### Researcher ###
     rname = db.Column(db.String(100), index=True)
@@ -39,43 +39,21 @@ class Metadata(db.Model):  # Address, SpatialExtent, TemporalExtent, Address):
         return Metadata(**d)
 
     @classmethod
-    def to_json(self, id=None):
+    def to_json(self):
         """Iterate over non-id, non-function, non-property attributes and pack
            them in to a dict for use in API
         """
-        # TODO use id to populate form for editing existing record
+        # print "\n\n***** TO JSON *****\n\n"
+        # print self.id
 
-        # each part has the form field name, field type, choices if they exist
+        return {
+                'id': self.id,
+                'title': self.title,
+                # TODO T_FMT = '%Y-%m-%d'
+                # 'date': self.date.strftime('%Y-%m-%d'),
+                # 'date': self.date,
+                'rname': self.rname,
+                'rinst': self.rinst
+               }
 
-        # no need to convert to json; will be handled by flask-restful
-        return jsonify(dict(form=MD_FORM))
 
-
-# TODO
-#: metadata form dictionary with aux info for building client-side form
-MD_FORM = [
-    dict(blockTitle="Basic Information", blockLabel="basic-information",
-         expanded="true",
-         formElements=[
-
-             dict(label='Title', type='text', order=1,
-                  value='Title of my data', name='title',
-                  longDescription="E.g., Data for June 2014 Journal of Hydrology Paper"),
-             dict(label='Date', type='date', order=1, name='date',
-                  value='2015-01-01',  # TODO strfmt now()
-                  longDescription='Date data was last updated')]
-         ),
-
-        dict(blockTitle="Researcher Information", blockLabel="researcher-information",
-         expanded="true",
-         formElements=[
-
-             dict(label='Researcher Name', type='text', order=1, name='rname',
-                  value='Professor Herod Jones',
-                  longDescription='Principal Investigator (may be one of many)'),
-
-             dict(label='Researcher Institute', type='text', order=1,
-                  name='rinst',
-                  value='University of Idaho',  # TODO strfmt now()
-                  longDescription='Home institute during data acquisition')]
-         )]
