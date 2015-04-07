8db4e00c3ca0f3a471f9bfd5baa51f5a6b38ba59
Matthew Turner <maturner01@gmail.com>
2015-04-07 10:16:21 -0700

    added all but two of the fields on server side, showing successfully on client

--- app/api/views.py
+++ app/api/views.py
@@ -158,36 +158,168 @@ def _make_mdform(val_dict=None, id=None):
             (list) of metadata blocks for display in Bootstrap panels
 
     """
-    if not val_dict:
-        val_dict = {'title': 'Title of my data',
-                    'date': '2015-01-01',
-                    'rname': 'Professor Herod Jones',
-                    'rinst': 'University of Idaho'}
+    topic_categories = \
+        ['Biota', 'Boundaries', 'Climatology/Meterology/Atmosphere',
+         'Economy', 'Elevation', 'Environment', 'Farming',
+         'Geoscientific Information', 'Health',
+         'Imagery/Base Maps/Earth Cover', 'Inland Waters', 'Location',
+         'Military Intelligence', 'Oceans', 'Planning/Cadastre', 'Society',
+         'Structure', 'Transportation', 'Utilities/Communication']
 
-    return [dict(blockTitle="Basic Information",
-            blockLabel="basic-information",
-            expanded="false",
-            formElements=[
-                dict(label='Title', type='text', order=1,
-                     value=val_dict['title'], name='title',
-                     longDescription="E.g., Data for June 2014 Journal of Hydrology Paper"),
-                dict(label='Date', type='date', order=1, name='date',
-                     value=val_dict['date'],
-                     longDescription='Date data was last updated'),
-                dict(type='hidden', id='hidden-form-id', value=id)
-                ]
+    if not val_dict:
+        today = datetime.today()
+        today = datetime.strftime(today, '%Y-%m-%d')
+
+        val_dict = {'title': 'Title of my data',
+                    'first-published-date': '2015-01-01',
+                    'last-modified-date': today,
+                    'citation-name': 'Professor Jimmy Jones',
+                    'citation-org': 'University of Idaho',
+                    'citation-address': '',
+                    'citation-city': '',
+                    'citation-state': '',
+                    'citation-zipcode': '',
+                    'citation-country': 'USA',
+                    'citation-phone': '',
+                    'citation-summary': '',
+                    'topic-category': topic_categories,
+                    'theme-keywords': 'water, drought',
+                    'place-keywords': 'Great Basin, High Plateau',
+                    'status': ['completed', 'continually updated',
+                               'in progress', 'planned'],
+                    'access-org': 'Northwest Knowledge Network',
+                    'access-name': 'Dr. Luke Sheneman',
+                    'access-address': '875 Perimeter Drive MS 2358',
+                    'access-city': 'Moscow',
+                    'access-state': 'ID',
+                    'access-country': 'USA',
+                    'access-phone': '(208) 885-2080',
+                    'access-email': 'info@northwestknowledge.net',
+                    'access-online': 'www.northwestknowledge.net',
+                    'access-zipcode': '83843'
+                    }
+
+    return [
+        dict(blockTitle="Basic Information",
+             blockLabel="basic-information",
+             expanded="false",
+             formElements=[
+             dict(label='Title', type='text', order=1,
+                  value=val_dict['title'], name='title',
+                  longDescription="E.g., Data for June 2014 Journal of Hydrology Paper"),
+
+             dict(label='Date Published', type='date', order=1, name='first-published-date',
+                  value=val_dict['first-published-date'],
+                  longDescription='Date data was first published'),
+
+             dict(label='Date Last Modified', type='date', order=1, name='last-modified-date',
+                  value=val_dict['last-modified-date'],
+                  longDescription='Date data or metadata was last updated'),
+
+            dict(type='hidden', id='hidden-form-id', value=id)
+            ]
          ),
 
-        dict(blockTitle="RInformation", blockLabel="researcher-information",
-         expanded="true",
-         formElements=[
+        dict(blockTitle="Data Summary",
+             blockLabel="data-information",
+             expanded="false",
+             formElements=[
 
-             dict(label='Researcher Name', type='text', order=1, name='rname',
-                  value=val_dict['rname'],
+             dict(label='Summary', type='text', name='summary',
+                  value=val_dict['citation-summary'],
+                  longDescription='Summary of the data'),
+
+             dict(label='Thematic Keywords', type='text', name='thematic-keywords',
+                 value=val_dict['theme-keywords'],
+                 longDescription='Comma-separated list of keywords that do not' +\
+                                 ' describe location'),
+
+             dict(label='Place Keywords', type='text', name='place-keywords',
+                 value=val_dict['place-keywords'],
+                 longDescription='Comma-separated list of keywords that' +\
+                                 ' describe location')
+             ]
+        ),
+
+        dict(blockTitle="Citation Information",
+             blockLabel="citation-information",
+             expanded="true",
+             formElements=[
+
+             dict(label='Name', type='text', order=1, name='citation-name',
+                  value=val_dict['citation-name'],
                   longDescription='Principal Investigator (may be one of many)'),
 
-             dict(label='Researcher Institute', type='text', order=1,
-                  name='rinst',
-                  value=val_dict['rinst'],  # TODO strfmt now()
-                  longDescription='Home institute during data acquisition')]
-         )]
+             dict(label='Organization/Affiliation', type='text', order=1,
+                  name='citation-org',
+                  value=val_dict['citation-org'],  # TODO strfmt now()
+                  longDescription='Home institute during data acquisition'),
+
+             dict(label='Address', type='text', name='citation-address',
+                  value=val_dict['citation-address'],
+                  longDescription='Organization\'s address'),
+
+             dict(label='City', type='text', name='citation-city',
+                  value=val_dict['citation-city'],
+                  longDescription='Organization City'),
+
+             dict(label='State', type='text', name='citation-state',
+                  value=val_dict['citation-state'],
+                  longDescription='Organization State'),
+
+             dict(label='Country', type='text', name='citation-country',
+                  value=val_dict['citation-country'],
+                  longDescription='Organization Country'),
+
+             dict(label='Zip Code', type='text', name='citation-zipcode',
+                  value=val_dict['citation-zipcode'],
+                  longDescription='Organization Zipcode'),
+
+             dict(label='Phone Number', type='text', name='citation-phone',
+                  value=val_dict['citation-phone'],
+                  longDescription='Organization Phone Number')
+             ]
+         ),
+
+        dict(blockTitle="Data Access Information",
+             blockLabel="access-information",
+             expanded="true",
+             formElements=[
+
+             dict(label='Name', type='text', order=1, name='access-name',
+                  value=val_dict['access-name'],
+                  longDescription='Point of contact for data storage'),
+
+             dict(label='Organization/Affiliation', type='text', order=1,
+                  name='access-org',
+                  value=val_dict['access-org'],  # TODO strfmt now()
+                  longDescription='Organization that is storing the data'),
+
+             dict(label='Address', type='text', name='access-address',
+                  value=val_dict['access-address'],
+                  longDescription='Data storage organization\'s address'),
+
+             dict(label='City', type='text', name='access-city',
+                  value=val_dict['access-city'],
+                  longDescription='Data storage organization\'s city'),
+
+             dict(label='State', type='text', name='access-state',
+                  value=val_dict['access-state'],
+                  longDescription='Data storage organization state'),
+
+             dict(label='Country', type='text', name='access-country',
+                  value=val_dict['access-country'],
+                  longDescription='Data storage organization country'),
+
+             dict(label='Zip Code', type='text', name='access-zipcode',
+                  value=val_dict['access-zipcode'],
+                  longDescription='Data storage organization zipcode'),
+
+             dict(label='Phone Number', type='text', name='access-phone',
+                  value=val_dict['access-phone'],
+                  longDescription='Data storage organization phone number')
+             ]
+         )
+
+
+        ]
