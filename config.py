4bc7aefabeccfa7692e6bfe5c801ab793a669333
Matthew Turner <maturner01@gmail.com>
2015-06-15 21:58:59 -0700

    something wrong starting up server, working now

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('VW_SECRET_KEY') or 'hard to guess string'
-    MONGODB_SETTINGS = {'DB': "metadata"}
+    MONGODB_SETTINGS = {'db': "metadata"}
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
