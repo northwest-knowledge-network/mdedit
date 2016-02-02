1deb5a8a16b5f5487d075e979ad363170f30d274
mguarinello <mguarinello@uidaho.edu>
2016-02-02 13:00:17 -0800

    enabled confirm functionality for delete

--- frontend/js/directives.js
+++ frontend/js/directives.js
@@ -17,4 +17,19 @@ metadataEditorApp.directive('fileModel', ['$parse', function ($parse) {
             });
         }
     };
+}])
+.directive('ngConfirmClick',[function(){
+    return {
+        priority: -1,
+        restrict: 'A',
+        link: function(scope, element, attrs){
+            element.bind('click', function(e){
+                var message = attrs.ngConfirmClick;
+                if(message && !confirm(message)){
+                    e.stopImmediatePropagation();
+                    e.preventDefault();
+                }
+            });
+        }
+    }
 }]);
