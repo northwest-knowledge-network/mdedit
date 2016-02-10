ddcc112a1ebf8184bc796dcde014b906689b4a39
Matthew Turner <maturner01@gmail.com>
2016-02-10 12:27:32 -0800

    using only nknportaldev for uploads

--- frontend/js/services.js
+++ frontend/js/services.js
@@ -517,17 +517,17 @@ metadataEditorApp
         comment out following block and uncomment the next block to
         test with NKN resources
     */
-    var uploadUrl;
-    if (hostname === 'localhost:4000') {
-        uploadUrl = 'http://localhost:4000/api/upload';
-    }
-    else {
-        uploadUrl =
-            'https://nknportal-dev.nkn.uidaho.edu/portal/simpleUpload/upload.php';
-    }
+    //var uploadUrl;
+    //if (hostname === 'localhost:4000') {
+        //uploadUrl = 'http://localhost:4000/api/upload';
+    //}
+    //else {
+        //uploadUrl =
+            //'https://nknportal-dev.nkn.uidaho.edu/portal/simpleUpload/upload.php';
+    //}
     /**** COMMENT OUT ABOVE, UNCOMMENT BELOW TO TEST YOUR REMOTE SERVER ***/
-    //var uploadUrl =
-        //'https://nknportal-dev.nkn.uidaho.edu/portal/simpleUpload/upload.php';
+    var uploadUrl =
+        'https://nknportal-dev.nkn.uidaho.edu/portal/simpleUpload/upload.php';
 
     var attachBaseRoute;
     if (hostname !== 'localhost:4000') {
