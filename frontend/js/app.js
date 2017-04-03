d7f665f2f45efd3287c911092f9e0ed0a6a4079b
Casey <ccasey645@hotmail.com>
2017-04-03 12:17:05 -0700

    Added all possible URL's.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -96,8 +96,8 @@ var metadataEditorApp = angular
 	envServiceProvider.config({
 	    domains: {
 		development: ['localhost', 'localhost:8000', 'locahost:8000/frontend'],
-		developmentServer: [ 'https://nkn-dev/nkn.uidaho.edu', 'https://nkn-dev.nkn.uidaho.edu/metadata-editor' ],
-		production: ['https://northwestknowledge.net/metadata-editor']
+		developmentServer: [ 'https://nkn-dev.nkn.uidaho.edu', 'https://nkn-dev.nkn.uidaho.edu/metadata-editor', 'https://nkn-dev.nkn.uidaho.edu/metadata-editor/', 'https://nkn-dev.nkn.uidaho.edu/metadata-editor#/iso', 'https://nkn-dev.nkn.uidaho.edu/metadata-editor#/dublin'],
+		production: ['https://northwestknowledge.net/metadata-editor', 'https://northwestknowledge.net/metadata-editor/', 'https://northwestknowledge.net/metadata-editor#/iso', 'https://northwestknowledge.net/metadata-editor#/dublin']
 	    },
 	    vars: {
 		development: {
